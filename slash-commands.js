2bc9c1b9901cb3f2ca0f63bbabd705b8cf3bb313
Nick <nlchan168@yahoo.com>
2024-03-08 23:07:55 -0500

    fixed slash command register

--- slash-commands.js
+++ slash-commands.js
@@ -1,4 +1,6 @@
-const { REST, Routes } = require("discord.js");
+import { REST, Routes } from "discord.js";
+import { config } from "dotenv";
+config();
 
 const botID = "1215566926806523924";
 const serverID = "752730590658428938";
@@ -7,15 +9,14 @@ const TOKEN = process.env.DISCORD_TOKEN;
 const rest = new REST().setToken(TOKEN);
 const slashRegister = async () => {
   try {
-    await rest.put(Reoutes.applicationGuildCommands(botID, serverID)),
-      {
-        body: [
-          {
-            name: "devig",
-            description: "Devig using LegOdds, FinalOdds",
-          },
-        ],
-      };
+    await rest.put(Routes.applicationGuildCommands(botID, serverID), {
+      body: [
+        {
+          name: "devig",
+          description: "Devig using LegOdds, FinalOdds",
+        },
+      ],
+    });
   } catch (error) {
     console.error(error);
   }
