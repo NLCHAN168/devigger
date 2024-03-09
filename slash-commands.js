efca27770152bef0285f7e71011796513a410f3e
Nick <nlchan168@yahoo.com>
2024-03-09 05:57:21 -0500

    updated server id

--- slash-commands.js
+++ slash-commands.js
@@ -3,7 +3,7 @@ import { config } from "dotenv";
 config();
 
 const botID = "1215566926806523924";
-const serverID = "752730590658428938";
+const serverID = "706188887814438972";
 const TOKEN = process.env.DISCORD_TOKEN;
 
 const rest = new REST().setToken(TOKEN);
