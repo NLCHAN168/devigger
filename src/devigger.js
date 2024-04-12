96b2d5d7cc5c25675f64ca1d9c6b3331c9bd4513
Nick <nlchan168@yahoo.com>
2024-04-12 03:46:45 -0400

    renamed obj to golfer in devig function

--- src/devigger.js
+++ src/devigger.js
@@ -6,38 +6,41 @@ const endUrl = "DevigMethod=4&Args=ev_p,fo_o,kelly,dm";
 
 //devig all objects inside array.odds
 async function devig(response, evarray) {
-  for (let obj of response.odds) {
+  for (let golfer of response.odds) {
     //add event name to each obj
-    obj.event_name = response.event_name;
-    obj.market = response.market;
+    golfer.event_name = response.event_name;
+    golfer.market = response.market;
     //calls devig for golfer if odds exist for DG AND FD
-    if (obj.fanduel !== null && obj.datagolf.baseline_history_fit != null) {
+    if (
+      golfer.fanduel !== null &&
+      golfer.datagolf.baseline_history_fit != null
+    ) {
       let list = [
         "LegOdds",
-        obj.datagolf.baseline_history_fit,
+        golfer.datagolf.baseline_history_fit,
         "FinalOdds",
-        obj.fanduel,
+        golfer.fanduel,
       ];
       let queryString =
         baseUrl + generateDeviggerUrl(arrayToObjectBuilder(...list)) + endUrl;
       await fetch(queryString)
         .then((res) => res.json())
         .then((data) => {
-          obj.devig = data;
-          obj.event_name = response.event_name;
-          obj.market = response.market;
+          golfer.devig = data;
+          golfer.event_name = response.event_name;
+          golfer.market = response.market;
           //assess EV, if above threshold, push to evarray
           // TODO: Add edge case for pings that become higher EV
           if (obj.devig.Final.EV_Percentage > 0.1 && obj.pinged !== true) {
             evarray.push(obj);
-            obj.pinged = true;
+            golfer.pinged = true;
             console.log("EV: " + obj.devig.Final.EV_Percentage);
             console.log("finalodds for fd: " + obj.fanduel);
           }
           //if fair value odds is positive, add "+" to value
-          if (obj.devig.Final.FairValue_Odds > 0) {
-            obj.devig.Final.FairValue_Odds =
-              "+" + obj.devig.Final.FairValue_Odds;
+          if (golfer.devig.Final.FairValue_Odds > 0) {
+            golfer.devig.Final.FairValue_Odds =
+              "+" + golfer.devig.Final.FairValue_Odds;
           }
         });
     }
