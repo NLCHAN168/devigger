bffbcd63c529674bd7532d8001c023f8bc6dd65a
Nick <nlchan168@yahoo.com>
2024-04-19 01:56:43 -0400

    ts edits

--- src/datagolf.js
+++ src/datagolf.js
@@ -1,24 +1,36 @@
 import { config } from "dotenv";
 import { generateDeviggerUrl, arrayToObjectBuilder } from "./querybuilder.js";
+/** @typedef {import('./d.ts').DatagolfResponse} DatagolfResponse*/
+/** @typedef {import('./d.ts').ThreeballResponse} ThreeballResponse */
 
 config();
 
+/**
+ *
+ * @param {*} list
+ * @returns {Promise<DatagolfResponse>}
+ */
 export const outrightOdds = async (list) => {
   const baseUrl = "https://feeds.datagolf.com/betting-tools/outrights?";
   const endUrl = `&odds_format=american&file_format=json&key=${process.env.DG_TOKEN}`;
   const queryString =
     baseUrl + generateDeviggerUrl(arrayToObjectBuilder(...list)) + endUrl;
-  return await fetch(queryString).then((res) => {
+  return fetch(queryString).then((res) => {
     return res.json();
   });
 };
 
+/**
+ *
+ * @param {*} list
+ * @returns {Promise<ThreeballResponse>}
+ */
 export const matchup3ballOdds = async (list) => {
   const baseUrl = "https://feeds.datagolf.com/betting-tools/matchups?";
   const endUrl = `&odds_format=american&file_format=json&key=${process.env.DG_TOKEN}`;
   const queryString =
     baseUrl + generateDeviggerUrl(arrayToObjectBuilder(...list)) + endUrl;
-  return await fetch(queryString).then((res) => {
+  return fetch(queryString).then((res) => {
     return res.json();
   });
 };
